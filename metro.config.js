c469daa297e44495efc08d2d201d47e1ebb6ba1e
Justin Bublitz <jmb10031226@gmail.com>
2026-05-19 14:45:25 -0400

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,7 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
+config.resolver.sourceExts.push('bin');
 config.resolver.assetExts.push('bin');
 
 module.exports = config;
